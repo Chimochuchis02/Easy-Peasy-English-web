b73496d5f4059c9cbbd561a32dc7614a5d983b9f
Chimochuchis02 <angeldominguezlopez030206@gmail.com>
2026-04-28 14:03:36 -0600

    Carousel Initial

--- index.php
+++ index.php
@@ -1,17 +1,44 @@
 <?php include 'Layouts/header.php'; ?>
 
 <main class="container py-5 text-center">
-    <h1 class="display-3 text-primary fw-bold">Easy Peasy English</h1>
-    <p class="lead">Aprender inglés nunca fue tan sencillo.</p>
-    
-    <div class="card shadow-sm p-4 mt-4">
-        <h2 class="h4">Just a reminder to...</h2>
-        <ul class="list-unstyled">
-            <li class="text-info">Always drink water...</li>
-            <li class="text-success">Gym training and...</li>
-            <li class="text-muted">Just be yourself</li>
-        </ul>
+    <div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
     </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 </main>
 
 <?php include 'Layouts/footer.php'; ?>
